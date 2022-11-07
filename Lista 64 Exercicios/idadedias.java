93402a9ff9eb4a92e03cf5f180c896c9c3773497
João Alves <81262157+joaoalves70@users.noreply.github.com>
2022-11-07 14:06:12 -0300

    Create idadedias.java

---
+++ Lista 64 Exercicios/idadedias.java
@@ -0,0 +1,23 @@
+/*****************************************************************
+ * 9) Faça um algoritmo que leia a idade de uma pessoa expressa em
+ * anos, meses e dias e escreva a idade dessa pessoa expressa apenas
+ * em dias. Considerando o ano com 365 dias e os meses com 30 dias.
+ ****************************************************************/
+
+import java.util.Scanner;
+public class Idadedias {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		double anos, meses, dias, diastotal;
+		
+		System.out.println("Informe sua idade em anos, meses e dias respectivamente: ");
+		anos = ler.nextDouble();
+		meses = ler.nextDouble();
+		dias = ler.nextDouble();
+		
+		diastotal = anos*365 + meses*30 + dias;
+		
+		System.out.print("Sua idade em dias é de: "+diastotal);
+	}
+}
