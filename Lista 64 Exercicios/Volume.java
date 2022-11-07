df2dc0defbe34d1a829215ec71fa22b8b0809c4a
João Alves <81262157+joaoalves70@users.noreply.github.com>
2022-11-07 13:57:37 -0300

    Create Volume.java

---
+++ Lista 64 Exercicios/Volume.java
@@ -0,0 +1,24 @@
+/*****************************************************************
+ * 8) Calcular e representar o valor do volume de uma lata de óleo 
+ * V = 3.14159*R*R*A
+ * onde as variaveis: V, R e A representam respectivamente o:
+ * Volume, o raio e a altura.
+ ****************************************************************/
+
+import java.util.Scanner;
+public class Volume {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		double v, r, a;
+		
+		System.out.print("Determine o valor do raio, e depois o da altura: ");
+		r = ler.nextDouble();
+		a = ler.nextDouble();
+		
+		v = 3.14159*r*r*a;
+		System.out.print("O volume da lata é de: "+v);
+		
+	}
+
+}
