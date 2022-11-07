259310d177024b09cc8464352ea6487e3167b1ee
João Alves <81262157+joaoalves70@users.noreply.github.com>
2022-11-07 13:37:40 -0300

    Create Fahrenheit.java

---
+++ Lista 64 Exercicios/Fahrenheit.java
@@ -0,0 +1,20 @@
+/*************************************************************
+ * 7) Ler uma temperatura em graus Fahrenheit e apresentá-la
+ * convertida em graus Celsius.
+ ************************************************************/
+
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		double f, c;
+		
+		System.out.print("Informe a temperatura em Fahrenheit: ");
+		f = ler.nextDouble();
+		
+		c = (f-32)*5/9;
+		
+		System.out.print("A temperatura em graus Celsius é de: "+c);
+	}
+}
