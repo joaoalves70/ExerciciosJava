04b7cddbc8ffd9e3044d6f1445ff0327da29ba92
João Alves <81262157+joaoalves70@users.noreply.github.com>
2022-11-09 12:37:28 -0300

    Create Celsius.java

---
+++ Lista 64 Exercicios/Celsius.java
@@ -0,0 +1,21 @@
+/*****************************************************************************
+ *  6) Ler uma temperatura em graus Celsius e apresentá-la convertida em graus
+ *  Fahrenheit.
+ ****************************************************************************/
+
+import java.util.Scanner;
+public class Celsius
+{
+	public static void main(String[] args) {
+		Scanner ler = new Scanner (System.in);
+		
+		double c, f;
+		
+		System.out.print("Informe a temperatura em graus celsius: ");
+		c = ler.nextDouble();
+		
+		f = ((c*1.8)+32);
+		
+		System.out.print(c+"°C equivale a: "+f+"°F.");
+	}
+}   
