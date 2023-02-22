471871eb542836d3f7ba84800cb29aaef4f94fb8
João Alves <81262157+joaoalves70@users.noreply.github.com>
2023-02-22 13:52:04 -0300

    Create Decescente.java

---
+++ Lista 64 Exercicios/Decescente.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+* 13) Escreva um programa que leia 3 números inteiros e imprima na tela os
+* valores em ordem decrescente.
+*******************************************************************************/
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner Ler = new Scanner (System.in);
+        int Quantidade = 3;
+        int[] Numeros = new int[Quantidade];
+        System.out.print("Ordenador de números\n\n");
+        
+        for (int x = 0; x < Quantidade; x++) {
+          System.out.print("Digite o "+(x+1)+" número:  ");
+          Numeros[x] = Ler.nextInt();
+        }
+        
+        Arrays.sort(Numeros);
+        
+        System.out.println("\n\nOs números em ordem crescente são:");
+        for (int Numero : Numeros) {
+          System.out.println(Numero);
+        }
+    }
+}
