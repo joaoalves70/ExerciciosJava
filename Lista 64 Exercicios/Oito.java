764b0c58a6babb6f71de7bb85431b7ee966ab76a
João Alves <81262157+joaoalves70@users.noreply.github.com>
2023-05-19 14:57:39 -0300

    Update and rename Volume.java to Oito.java

--- Lista 64 Exercicios/Oito.java
+++ Lista 64 Exercicios/Oito.java
@@ -6,7 +6,7 @@
  ****************************************************************/
 
 import java.util.Scanner;
-public class Volume {
+public class Oito {
 
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
