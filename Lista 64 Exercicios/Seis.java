945b1d9443a148aaa60feddd8c404a667692495a
João Alves <81262157+joaoalves70@users.noreply.github.com>
2023-05-19 14:30:31 -0300

    Update and rename Celsius.java to Seis.java

--- Lista 64 Exercicios/Seis.java
+++ Lista 64 Exercicios/Seis.java
@@ -4,7 +4,7 @@
  ****************************************************************************/
 
 import java.util.Scanner;
-public class Celsius
+public class Seis
 {
 	public static void main(String[] args) {
 		Scanner ler = new Scanner (System.in);
