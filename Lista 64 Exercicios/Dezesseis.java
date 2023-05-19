c12a31e2d083aebe046d5b61666c97704cf99983
João Alves <81262157+joaoalves70@users.noreply.github.com>
2023-05-19 14:45:08 -0300

    Update and rename NumerosIguais.java to Dezesseis.java

--- Lista 64 Exercicios/Dezesseis.java
+++ Lista 64 Exercicios/Dezesseis.java
@@ -6,7 +6,7 @@
 
 import java.util.Scanner;
 
-public class NumerosIguais {
+public class Dezesseis {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
