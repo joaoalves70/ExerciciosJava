94e7039b2766509eb49a0a76878b712b5163864d
João Alves <81262157+joaoalves70@users.noreply.github.com>
2023-05-19 14:58:03 -0300

    Update and rename idadedias.java to Nove.java

--- Lista 64 Exercicios/Nove.java
+++ Lista 64 Exercicios/Nove.java
@@ -5,7 +5,7 @@
  ****************************************************************/
 
 import java.util.Scanner;
-public class Idadedias {
+public class Nove {
 
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
