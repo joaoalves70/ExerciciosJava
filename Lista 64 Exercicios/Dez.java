290c443bcade91565aed86162a7016af1a695028
João Alves <81262157+joaoalves70@users.noreply.github.com>
2023-05-19 14:31:42 -0300

    Update and rename Comparacoes.java to Dez.java

--- Lista 64 Exercicios/Dez.java
+++ Lista 64 Exercicios/Dez.java
@@ -6,7 +6,7 @@
  ******************************************************************/
 
 import java.util.Scanner;
-public class Main {
+public class Dez {
 
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
