2c1a71a1c9588cffc7fbd4fbfd44b3236b9ed7f2
João Alves <81262157+joaoalves70@users.noreply.github.com>
2023-05-22 13:15:40 -0300

    Update VinteNove.java

--- Lista 64 Exercicios/VinteNove.java
+++ Lista 64 Exercicios/VinteNove.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
 * 29) Faça um programa que exiba os números de 1 até 2000.
 *******************************************************************************/
-public class Exercicio {
+public class VinteNove {
 
   public static void main(String[] args) {
 
