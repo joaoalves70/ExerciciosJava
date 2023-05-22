d7f093f6b30e8522a35d23a934930e57b286115b
João Alves <81262157+joaoalves70@users.noreply.github.com>
2023-05-22 13:14:56 -0300

    Update VinteCinco.java

--- Lista 64 Exercicios/VinteCinco.java
+++ Lista 64 Exercicios/VinteCinco.java
@@ -11,7 +11,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Exercicio {
+public class VinteCinco {
 
     public static void main(String[] args) {
       Scanner entrada = new Scanner(System.in);
