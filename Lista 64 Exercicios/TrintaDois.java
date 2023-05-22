ae857244d212b2830589f6337a9e1f9979494541
João Alves <81262157+joaoalves70@users.noreply.github.com>
2023-05-22 13:16:19 -0300

    Update TrintaDois.java

--- Lista 64 Exercicios/TrintaDois.java
+++ Lista 64 Exercicios/TrintaDois.java
@@ -5,7 +5,7 @@
 *******************************************************************************/
 import java.util.Scanner;
 
-public class Exercicio {
+public class VinteDois {
 
 	public static void main(String[] args) {
   
