999eff6548c67354dfd501b22a05db918a89a65d
João Alves <81262157+joaoalves70@users.noreply.github.com>
2023-05-22 13:16:00 -0300

    Update Trinta.java

--- Lista 64 Exercicios/Trinta.java
+++ Lista 64 Exercicios/Trinta.java
@@ -3,7 +3,7 @@
 *******************************************************************************/
 import java.util.Scanner;
 
-public class Exercicio {
+public class Trinta {
 
   public static void main(String[] args) {
     Scanner entrada = new Scanner(System.in);
